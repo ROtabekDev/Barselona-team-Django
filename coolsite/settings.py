77931cf08e554b0e79aac9fcb0dc37bde747258d
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-19 14:03:36 +0500

    Add Players model

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)z^(%+e#nq!=^3drxhmsf8+5zp$da^w0zahe3u=+84h-rub69g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- coolsite/urls.py
+++ coolsite/urls.py
@@ -1,5 +1,7 @@
 
 
+from django.conf.urls.static import static
+from coolsite import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,5 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("players.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
