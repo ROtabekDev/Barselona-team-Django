1a60e1ecf06a5fd6a071c15e5bea3d8233f03235
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-19 13:41:48 +0500

    Маршрутизация, обработка исключений запросов, перенаправления

--- players/views.py
+++ players/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Bu yangi app.")
+
+def categories(request, catid): 
+    return HttpResponse(f"<h1>Kategoriyalar</h1><p>{catid}</p>")
+
+def archive(request, year): 
+    if int(year)>2022: 
+        return redirect('home', permanent=False)
+
+    return HttpResponse(f"<h1>Arxiv yillar</h1><p>{year}</p>")
+
+def pageNotFound(request, exception):
+    return HttpResponse('<h1>Afsus sahifa topilmadi. </h1>')
+
