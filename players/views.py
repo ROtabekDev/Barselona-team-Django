9a48bb803093196cfb5388d1b242c3d6e5028569
ROtabekDev <otabekrahmonovtest@gmail.com>
2022-05-20 10:31:22 +0500

    URL

--- players/views.py
+++ players/views.py
@@ -4,24 +4,36 @@ from django.http import Http404, HttpResponse
 
 from .models import Players
 
-menu = ["Biz haqimizda", "Post qo`shish", "Bog`lanish", "Chiqish"]
+menu = [{'title': 'Biz haqimizda', 'url_name': 'about'},
+        {'title': 'Post qo`shish', 'url_name': 'add_page'},
+        {'title': 'Bog`lanish', 'url_name': 'contact'},
+        {'title': 'Chiqish', 'url_name': 'login'},
+]
 
 def index(request):
     posts = Players.objects.all()
-    return render(request, 'players/index.html', {'posts': posts,'menu': menu, 'title': 'Asosiy sahifa'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Asosiy sahifa'
+    }
+    return render(request, 'players/index.html', context=context)
 
 def about(request):
     return render(request, 'players/about.html', {'menu': menu, 'title': 'Biz haqimizda'})
     
-def categories(request, catid): 
-    return HttpResponse(f"<h1>Kategoriyalar</h1><p>{catid}</p>")
+def addpage(request): 
+    return HttpResponse("Post qo`shish")
 
-def archive(request, year): 
-    if int(year)>2022: 
-        return redirect('home', permanent=False)
+def contact(request): 
+    return HttpResponse("Bog`lanish")
 
-    return HttpResponse(f"<h1>Arxiv yillar</h1><p>{year}</p>")
+def login(request): 
+    return HttpResponse("Avtorizatsiya")
 
 def pageNotFound(request, exception):
     return HttpResponse('<h1>Afsus sahifa topilmadi. </h1>')
+
+def show_post(request, post_id):
+    return HttpResponse(f"Id: {post_id}")
 
